8c1d5bde5dc3fb83f9559a441db0efe75d2154e5
Michael Waskom <mwaskom@nyu.edu>
2020-06-17 18:30:13 -0400

    Implement solution removal with push back to feature branch

--- ci/process_notebooks.py
+++ ci/process_notebooks.py
@@ -1,3 +1,10 @@
+"""Process tutorials for Neuromatch Academy
+
+- Excute .ipynb files and report any errors encountered
+- Copy the original notebook to a "solutions" folder for TAs
+- Remove inputs (but not outputs) from solution cells in original notebook
+
+"""
 import os
 import sys
 import argparse
@@ -6,12 +13,12 @@ from nbconvert.preprocessors import ExecutePreprocessor
 
 
 def main(arglist):
-
+    """Process IPython notebooks from a list of files."""
     args = parse_args(arglist)
 
     # Filter to only ipython notebook fikes
-    nb_files = [arg for arg in args.files if arg.endswith(".ipynb")]
-    if not nb_files:
+    nb_paths = [arg for arg in args.files if arg.endswith(".ipynb")]
+    if not nb_paths:
         print("No notebook files found")
         sys.exit(0)
 
@@ -20,30 +27,79 @@ def main(arglist):
     if "NB_KERNEL" in os.environ:
         exec_kws["kernel_name"] = os.environ["NB_KERNEL"]
 
+    # Defer failures until after processing all notebooks
     errors = {}
+    notebooks = {}
 
-    for nb_file in nb_files:
+    for nb_path in nb_paths:
 
         # Load the notebook structure
-        with open(nb_file) as f:
+        with open(nb_path) as f:
             nb = nbformat.read(f, nbformat.NO_CONVERT)
 
-        # Run the notebook from top to bottom
-        print(f"Executing {nb_file}")
+        # Run the notebook from top to bottom, catching errors
+        print(f"Executing {nb_path}")
         executor = ExecutePreprocessor(**exec_kws)
         try:
             executor.preprocess(nb)
         except Exception as err:
-            errors[nb_file] = err
+            errors[nb_path] = err
+        else:
+            notebooks[nb_path] = nb
 
     if args.checkonly:
         exit(errors)
 
+    # Check compliancy with PEP8, generate a report, but don't fail on issues
+
+    # TODO Check notebook name format?
+
+    # Save the full version of the notebook, which contains solutions
+    for nb_path, nb in notebooks.items():
+
+        nb_dir, nb_fname = os.path.split(nb_path)
+
+        # TODO only save out a solutions notebook if some solutions exist?
+
+        # Create a directory for the solutions, if it doesn't exist
+        solutions_dir = os.path.join(nb_dir, "solutions")
+        if not os.path.exists(solutions_dir):
+            os.mkdir(solutions_dir)
+
+        # Write the full notebook (TA verison) to the solutions directory
+        solutions_path = os.path.join(solutions_dir, nb_fname)
+        print(f"Writing TA notebook to {solutions_path}")
+        with open(solutions_path, "w") as f:
+            nbformat.write(nb, f)
+
+        # Remove solutions and write the student version of the notebook
+        remove_solutions(nb_path, nb)
+
     exit(errors)
 
 
-def exit(errors):
+def remove_solutions(nb_path, nb):
+    """Remove input source from solution cells with output, or remove cell."""
+    print(f"Removing solutions from {nb_path}")
 
+    # Remove the solutions, but keep images they generate
+    removed_message = "# Solution removed, here is the example output:"
+    nb_cells = nb.get("cells", [])
+    for cell in nb_cells:
+        cell_text = cell["source"].replace(" ", "").lower()
+        if cell_text.startswith("#@titlesolution"):
+            if cell["outputs"]:
+                cell["source"] = removed_message
+            else:
+                nb_cells.remove(cell)
+
+    # Write the processed notebook back out to the original path
+    with open(nb_path, "w") as f:
+        nbformat.write(nb, f)
+
+
+def exit(errors):
+    """Exit with message and status dependent on contents of erros dictionary."""
     for failed_file, error in errors.items():
         print(f"{failed_file} did not execute cleanly.")
         print("Error message:", end="\n")
@@ -58,7 +114,7 @@ def exit(errors):
 
 
 def parse_args(arglist):
-
+    """Handle the command-line arguments."""
     parser = argparse.ArgumentParser(
         description="Process neuromatch tutorial notebooks",
     )
