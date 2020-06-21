1866bf12ac8ea16e62bcdd2f07028a5c35c29938
Michael Waskom <mwaskom@nyu.edu>
2020-06-21 15:39:13 -0400

    Fix code that redirects Colab badge url

--- ci/process_notebooks.py
+++ ci/process_notebooks.py
@@ -178,7 +178,7 @@ def redirect_colab_badge(cell):
     """Modify the Colab badge to point at the master branch on Github."""
     cell_text = cell["source"]
     p = re.compile(r"^(.+/NeuromatchAcademy/course-content/blob/)\w+(/.+$)")
-    return p.sub(r"\1master\2", cell_text)
+    cell["source"] = p.sub(r"\1master\2", cell_text)
 
 
 def sequentially_executed(nb):
