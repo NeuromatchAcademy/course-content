c4f35b977c93dd5758a1bdd29a5a5ffb3884b313
Michael Waskom <mwaskom@nyu.edu>
2020-06-27 18:14:14 -0400

    Test README writer

--- ci/generate_tutorial_readme.py
+++ ci/generate_tutorial_readme.py
@@ -1,22 +1,26 @@
+"""Write a directory of tutorial notebooks to the README file.
+
+Run this script from the root of the github repository.
+
+"""
 import os
 from glob import glob
 
 
 def main():
 
-    readme_text = [
-        "# Tutorials",
+    course_readme_text = [
+        "# Neuromatch Academy Tutorial Notebooks",
         "",
-        "Tutorial notebooks are stored here, organized by each day."
-        ""
     ]
 
-    day_paths = sorted(glob("tutorials/W?W?_*"))
+    day_paths = sorted(glob("tutorials/W?D?_*"))
     for day_path in day_paths:
 
         day_name = os.path.split(day_path)[-1]
         day_code, topic = day_name.split("_")
 
+        # Split the UpperCamelCase topic name into separate words
         topic_words = []
         for letter in topic:
             if letter.isupper():
@@ -25,63 +29,103 @@ def main():
                 topic_words[-1] += letter
         topic = " ".join(topic_words)
 
-        readme_text.extend([
-            f"## {day_code} - {topic}"
-            ""
-        ])
-
-        readme_text.extend({
-            "### Tutorial notebooks"
-            "",
-            "|   | Student version | Instructor version |",
-            "| - | --------------- | ------------------ |",
-        })
-
         # Note: this will fail if we have 10 notebooks
         notebooks = sorted(glob(f"{day_path}/*.ipynb"))
-        for i, instructor_nb in enumerate(notebooks, 1):
-            _, nb_fname = os.path.split(instructor_nb)
-            student_nb = f"{day_path}/student/{nb_fname}"
 
-            student_badge = make_colab_badge(student_nb)
-            instructor_badge = make_colab_badge(instructor_nb)
+        if not notebooks:
+            continue
 
-            readme_text.extend([
-                f"| Tutorial {i} | {student_badge} | {instructor_badge} |"
-            ])
+        student_notebooks = get_student_links(notebooks)
 
-        readme_text.append("\n")
+        # Build the table header
+        course_readme_text.extend([
+            f"## {day_code} - {topic}",
+            "",
+        ])
 
+        # Build the main table with student notebook links
+        course_readme_text.extend(write_badge_table(student_notebooks))
+        course_readme_text.append("\n")
+
+        # Now make the day-specific README
+        # with links to both instructor and student versions
+        day_readme_text = [
+            f"# {day_code} - {topic}",
+            "",
+            "## Instructor notebooks",
+            "",
+        ]
+        day_readme_text.extend(write_badge_table(notebooks))
+
+        day_readme_text.extend([
+            "## Student notebooks",
+            "",
+        ])
+        day_readme_text.extend(write_badge_table(student_notebooks))
+
+        # Write the course README file
+        with open(f"{day_path}/README.md", "w") as f:
+            f.write("\n".join(day_readme_text))
+
+    # Write the course README file
     with open("tutorials/README.md", "w") as f:
+        f.write("\n".join(course_readme_text))
 
-        f.write("\n".join(readme_text))
+
+def write_badge_table(notebooks):
+
+    table_text = [
+        "|   | Runnable | Static |",
+        "| - | -------- | ------ |",
+    ]
+
+    # Add each row of the table
+    for i, local_path in enumerate(notebooks, 1):
+
+        colab_badge = make_colab_badge(local_path)
+        nbviewer_badge = make_nbviewer_badge(local_path)
+        table_text.append(
+            f"| Tutorial {i} | {colab_badge} | {nbviewer_badge} |"
+        )
+    table_text.append("\n")
+
+    return table_text
 
 
-def make_colab_badge(github_path):
+def get_student_links(instructor_notebooks):
 
+    student_notebooks = []
+    for instructor_nb in instructor_notebooks:
+        day_path, nb_fname = os.path.split(instructor_nb)
+        student_notebooks.append(f"{day_path}/student/{nb_fname}")
+    return student_notebooks
+
+
+def make_colab_badge(local_path):
+    """Generate a Google Colaboratory badge for a notebook on github."""
     alt_text = "Open In Colab"
     badge_svg = "https://colab.research.google.com/assets/colab-badge.svg"
     url_base = (
         "https://colab.research.google.com/"
-        "github/NeuromatchAcademy/course-content/blob/master/"
+        "github/NeuromatchAcademy/course-content/blob/master"
     )
-    return make_badge(alt_text, badge_svg, url_base, github_path)
+    return make_badge(alt_text, badge_svg, url_base, local_path)
 
 
-def make_nbviewer_badge(github_path):
-
+def make_nbviewer_badge(local_path):
+    """Generate an NBViewer badge for a notebook on github."""
     alt_text = "View the notebook"
     badge_svg = "https://img.shields.io/badge/render-nbviewer-orange.svg"
     url_base = (
         "https://nbviewer.jupyter.org/"
-        "github/NeuromatchAcademy/course-content/blob/master/"
+        "github/NeuromatchAcademy/course-content/blob/master"
     )
-    return make_badge(alt_text, badge_svg, url_base, github_path)
+    return make_badge(alt_text, badge_svg, url_base, local_path)
 
 
-def make_badge(alt_text, badge_svg, url_base, github_path):
-
-    return f"[![{alt_text}]({badge_svg})]({url_base}/{github_path})"
+def make_badge(alt_text, badge_svg, url_base, local_path):
+    """Generate a markdown element for a badge image that links to a file."""
+    return f"[![{alt_text}]({badge_svg})]({url_base}/{local_path})"
 
 
 if __name__ == "__main__":
