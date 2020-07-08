288283dcd413e939b48e2a5745348a2e284c4fc2
GitHub Action <action@github.com>
2020-07-08 21:19:28 +0000

    Process tutorial notebooks

--- tutorials/W0D1_PythonWorkshop1/solutions/W0D1_Tutorial1_Solution_133978a8.py
+++ tutorials/W0D1_PythonWorkshop1/solutions/W0D1_Tutorial1_Solution_133978a8.py
@@ -3,7 +3,7 @@
 np.random.seed(2020)
 
 # initialize step_end, n and v_n
-step_end = int(t_max/dt)
+step_end = int(t_max / dt)
 n = 50
 v_n = [el] * n
 
@@ -16,11 +16,11 @@ plt.ylabel(r'$V_m$ (V)')
 # loop for step_end steps
 for step in range(step_end):
   t = step * dt
-  plt.plot([t]*n, v_n, 'k.', alpha=0.05)
+  plt.plot([t] * n, v_n, 'k.', alpha=0.05)
 
   # loop for n steps
   for j in range(0, n):
-    i = i_mean * (1 + 0.1*(t_max/dt)**(0.5)*(2*np.random.random()-1))
-    v_n[j] = v_n[j] + dt/tau * (el - v_n[j] + r*i)
+    i = i_mean * (1 + 0.1 * (t_max/dt)**(0.5) * (2* np.random.random() - 1))
+    v_n[j] = v_n[j] + (dt / tau) * (el - v_n[j] + r*i)
 
 plt.show()
--- tutorials/W0D1_PythonWorkshop1/solutions/W0D1_Tutorial1_Solution_a8652968.py
+++ tutorials/W0D1_PythonWorkshop1/solutions/W0D1_Tutorial1_Solution_a8652968.py
@@ -3,7 +3,7 @@
 np.random.seed(2020)
 
 # initialize step_end, n and v_n
-step_end = int(t_max/dt)
+step_end = int(t_max / dt)
 n = 50
 v_n = [el] * n
 
@@ -15,11 +15,11 @@ plt.ylabel(r'$V_m$ (V)')
 
 # loop for step_end steps
 for step in range(step_end):
-  t = step*dt
+  t = step * dt
 
   v_mean = sum(v_n) / n
   v_var_n = [(v - v_mean)**2 for v in v_n]
-  v_var = sum(v_var_n) / (n-1)
+  v_var = sum(v_var_n) / (n - 1)
   v_std = np.sqrt(v_var)
 
   plt.plot(n*[t], v_n, 'k.', alpha=0.05)
@@ -28,7 +28,7 @@ for step in range(step_end):
   plt.plot(t, v_mean - v_std, 'C7.', alpha=0.8)
 
   for j in range(0, n):
-    i = i_mean * (1 + 0.1*(t_max/dt)**(0.5)*(2*np.random.random()-1))
-    v_n[j] = v_n[j] + dt/tau * (el - v_n[j] + r*i)
+    i = i_mean * (1 + 0.1 * (t_max / dt)**(0.5) * (2 * np.random.random() - 1))
+    v_n[j] = v_n[j] + (dt / tau) * (el - v_n[j] + r*i)
 
 plt.show()
