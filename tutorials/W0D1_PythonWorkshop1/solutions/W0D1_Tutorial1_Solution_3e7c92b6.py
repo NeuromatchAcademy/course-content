ec007b6f1640c618ede2128d424effa42cb42d8b
Spiros Ch <spirosChv@users.noreply.github.com>
2020-07-09 00:39:54 +0300

    Adding finalized tutorial for W0D1 (#231)
    * Created using Colaboratory
    
    * Process tutorial notebooks
    
    * Add W0D1 youtube playlist
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>
    Co-authored-by: Michael Waskom <mwaskom@nyu.edu>

--- tutorials/W0D1_PythonWorkshop1/solutions/W0D1_Tutorial1_Solution_3e7c92b6.py
+++ tutorials/W0D1_PythonWorkshop1/solutions/W0D1_Tutorial1_Solution_3e7c92b6.py
@@ -7,9 +7,5 @@ v = el
 for step in range(step_end):
   t = step * dt
   i = i_mean * (1 + np.sin((t * 2 * np.pi) / 0.01))
-  print(f'{t:.3f} {v:.4e}') 
+  print(f"{t:.3f} {v:.4e}") 
   v = v + dt/tau * (el - v + r*i)
-
-# last step
-t = t + dt    
-print(f'{t:.3f} {v:.4e}')  
--- tutorials/W0D1_PythonWorkshop1/solutions/W0D1_Tutorial1_Solution_b24ee7b3.py
+++ tutorials/W0D1_PythonWorkshop1/solutions/W0D1_Tutorial1_Solution_b24ee7b3.py
@@ -3,10 +3,10 @@
 np.random.seed(2020)
 
 # initialize step_end, t_range, v and syn
-step_end = int(t_max/dt)
+step_end = int(t_max / dt)
 t_range = np.linspace(0, t_max, num=step_end)
 v = el * np.ones(step_end)
-syn = i_mean * (1 + 0.1*(t_max/dt)**(0.5)*(2*np.random.random(step_end)-1))
+syn = i_mean * (1 + 0.1 * (t_max / dt)**(0.5) * (2 * np.random.random(step_end) - 1))
 
 # loop for step_end values of syn
 for step, i in enumerate(syn):
@@ -15,7 +15,7 @@ for step, i in enumerate(syn):
   if step==0:
     continue
 
-  v[step] = v[step-1] + dt/tau * (el - v[step-1] + r*i)
+  v[step] = v[step - 1] + (dt / tau) * (el - v[step - 1] + r*i)
 
 with plt.xkcd():
   # initialize the figure
