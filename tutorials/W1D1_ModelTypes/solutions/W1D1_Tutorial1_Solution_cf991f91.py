b06ab706fa3fbe4aff8dfb3d81275fdf13b5aa4c
GitHub Action <action@github.com>
2020-07-08 06:37:00 +0000

    Process tutorial notebooks

--- tutorials/W1D1_ModelTypes/solutions/W1D1_Tutorial1_Solution_cf991f91.py
+++ tutorials/W1D1_ModelTypes/solutions/W1D1_Tutorial1_Solution_cf991f91.py
@@ -1,6 +1,8 @@
 single_neuron_idx = 283
 single_neuron_spikes = spike_times[single_neuron_idx]
 single_neuron_isis = np.diff(single_neuron_spikes)
+
+n_bins = 50
 with plt.xkcd():
   plt.hist(single_neuron_isis, n_bins, histtype="stepfilled")
   plt.xlabel("ISI duration (s)")
