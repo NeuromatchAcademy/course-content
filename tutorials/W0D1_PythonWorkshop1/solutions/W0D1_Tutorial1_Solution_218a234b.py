8d813a28b732f2c23f644de214f9a5a95da85f4c
raymond shpeley <rshpeley@ualberta.ca>
2020-07-09 15:30:06 -0600

    Update W0D1_Tutorial1_Solution_218a234b.py
    This exercise is meant to replicate exercise 7 but using numpy arrays in place of python lists. The solution for exercise 7 and its graph shows all 150 points are plotted. This solution plots 149 points and misses the first one. You'll see this visually in comparing the 2 graphs.
    
    The recommended code changes increase the size of the v array by one to accommodate the last calculated update to v and slices v back to size before plotting.
    
    The solution graph will also need to be updated.

--- tutorials/W0D1_PythonWorkshop1/solutions/W0D1_Tutorial1_Solution_218a234b.py
+++ tutorials/W0D1_PythonWorkshop1/solutions/W0D1_Tutorial1_Solution_218a234b.py
@@ -5,13 +5,13 @@ np.random.seed(2020)
 # initialize step_end, t_range, v and syn
 step_end = int(t_max / dt)
 t_range = np.linspace(0, t_max, num=step_end)
-v = el * np.ones(step_end)
+v = el * np.ones(step_end + 1)
 syn = i_mean * (1 + 0.1 * (t_max/dt) ** (0.5) * (2 * np.random.random(step_end) - 1))
 
 # loop for step_end - 1 steps
-for step in range(1, step_end):
+for step in range(step_end):
 
-  v[step] = v[step - 1] + (dt / tau) * (el - v[step - 1] + r * syn[step])
+  v[step + 1] = v[step] + (dt / tau) * (el - v[step] + r * syn[step])
 
 with plt.xkcd():
   # initialize the figure
@@ -20,5 +20,5 @@ with plt.xkcd():
   plt.xlabel('time (s)')
   plt.ylabel(r'$V_m$ (V)')
 
-  plt.plot(t_range, v, 'k.')
+  plt.plot(t_range, v[:-1], 'k.')
   plt.show()
