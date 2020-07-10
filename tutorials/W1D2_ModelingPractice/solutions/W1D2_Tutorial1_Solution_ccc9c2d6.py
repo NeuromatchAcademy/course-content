95db8cf109b5ce3ae057d9eabe26b98acac38b2c
Madineh Sarvestani <msarvestani@gmail.com>
2020-07-10 03:35:58 -0400

    Adding W1D2 notebooks (#239)
    * Waxed and Linted
    
    * Waxed and Linted
    
    * Update with W1D2 links
    
    * Update slide_links.txt
    
    * Update slide_links.txt
    
    * Process tutorial notebooks
    
    * Add W0D2 tutorials (#234)
    
    * Created using Colaboratory
    
    * Process tutorial notebooks
    
    * W0 d1 fixed typo (#235)
    
    * Created using Colaboratory
    
    * Process tutorial notebooks
    
    * Delete W0D1_Tutorial1.ipynb
    
    * Created using Colaboratory
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>
    
    * Add W0D2 youtube playlist
    
    * Process tutorial notebooks
    
    * Created using Colaboratory
    
    * Process tutorial notebooks
    
    * Hide code in youtube cells
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>
    Co-authored-by: Michael Waskom <mwaskom@nyu.edu>
    Co-authored-by: Michael Waskom <mwaskom@users.noreply.github.com>
    
    * Created using Colaboratory
    
    * Process tutorial notebooks
    
    * Created using Colaboratory
    
    * Process tutorial notebooks
    
    * Waxed and Linted - Final Videos
    
    * Waxed and Linted - Final Videos
    
    * Process tutorial notebooks
    
    * Waxed and Linted - w updated vidoes
    
    * Process tutorial notebooks
    
    * Updated W1D2 playlist
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>
    Co-authored-by: Spiros Ch <spirosChv@users.noreply.github.com>
    Co-authored-by: Michael Waskom <mwaskom@nyu.edu>
    Co-authored-by: Michael Waskom <mwaskom@users.noreply.github.com>

---
+++ tutorials/W1D2_ModelingPractice/solutions/W1D2_Tutorial1_Solution_ccc9c2d6.py
@@ -0,0 +1,30 @@
+
+
+def my_threshold(selfmotion_vel_est, threshold):
+  """
+  This function should calculate proportion self motion
+  for both conditions and the overall proportion
+  correct classifications.
+
+  Args:
+      selfmotion_vel_est (numpy.ndarray): A sequence of floats
+      indicating the estimated self motion for all trials.
+
+      threshold (float): A threshold for the estimate of self motion when
+      the brain decides there really is self motion.
+
+  Returns:
+      (numpy.ndarray): 3 floats with the proportion of trials classified as
+      having self motion in the world-motion condition, in the self-motion
+      condition, and the proportion correct.
+  """
+
+  ##############################################################
+  # Compare the self motion estimates to the threshold:
+  # Replace 'True' with the proper code:
+
+  is_move = (selfmotion_vel_est > threshold)  # should return true or false.
+
+  ##############################################################
+
+  return is_move
