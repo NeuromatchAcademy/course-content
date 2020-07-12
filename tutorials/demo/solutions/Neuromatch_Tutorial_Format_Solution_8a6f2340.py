5cb5633dbc17b2c38600b8573ebefdb80eb44c0c
Michael Waskom <mwaskom@users.noreply.github.com>
2020-07-12 18:14:38 -0400

    Put style guide back where it belongs (#268)
    * Put style guide back where it belongs
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>

---
+++ tutorials/demo/solutions/Neuromatch_Tutorial_Format_Solution_8a6f2340.py
@@ -0,0 +1,7 @@
+
+"""
+Discussion: Write a dicussion about/answers to any open-ended questions you pose (either about a demo or elsewhere)
+
+You can write a paragraph or two of nice didactic text
+within a single comment. 
+""";
