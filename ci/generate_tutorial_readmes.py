268bb7070e396abc124300d4361b4c0be941c935
Max Myroshnychenko <mmyros@gmail.com>
2020-07-14 10:11:32 -0400

    force flush_cache for nbviewer (#302)

--- ci/generate_tutorial_readmes.py
+++ ci/generate_tutorial_readmes.py
@@ -200,7 +200,7 @@ def make_nbviewer_badge(local_path):
         "https://nbviewer.jupyter.org/"
         "github/NeuromatchAcademy/course-content/blob/master"
     )
-    return make_badge(alt_text, badge_svg, url_base, local_path)
+    return make_badge(alt_text, badge_svg, url_base, local_path+'?flush_cache=true')
 
 
 def make_badge(alt_text, badge_svg, url_base, local_path):
