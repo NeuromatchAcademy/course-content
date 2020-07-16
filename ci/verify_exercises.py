beb451793a2efaaf170489d1bb778714ee9d6a92
Michael Waskom <mwaskom@nyu.edu>
2020-07-15 21:15:04 -0400

    Better documentation of exercise verification script

--- ci/verify_exercises.py
+++ ci/verify_exercises.py
@@ -1,4 +1,19 @@
 #! /usr/bin/env python
+"""Check that exercise code matches solution code.
+
+Exercises are allowed to deivate from solutions in several ways:
+
+- Exercise code may replace solution code with an ellipsis (...)
+- Exercise code may have "commented-out" solution code
+
+Additionally:
+
+- Docstrings are currently ignored
+- Blank lines are ignored
+
+This script will report whether exercises and solutions otherwise match.
+
+"""
 import re
 import sys
 from textwrap import dedent
@@ -8,53 +23,78 @@ import nbformat
 
 def main(nb_fpath):
 
+    # Track overall status
+    failure = False
+
+    # Track the ordinal exercise cell number
+    # (note, exercises may be labeled differently in the notebook)
+    exercise = 0
+
+    # Load the notebook file
     with open(nb_fpath) as f:
         nb = nbformat.read(f, nbformat.NO_CONVERT)
 
-    exercise = 0
     for i, cell in enumerate(nb.get("cells", [])):
 
+        # Detect solution cells based on removal tag
         if has_solution(cell):
-
             exercise += 1
 
+            # Find a corresponding exercise cell
+            # (Assume it is the previous *code* cell)
             j = 1
+            stub_cell = None
             while (i - j):
                 stub_cell = nb["cells"][i - j]
                 if stub_cell["cell_type"] == "code":
+                    stub_cell = None
                     break
                 j += 1
+            if stub_cell is None:
+                continue
 
+            # Extract the code and comments from both cells
             stub_code, stub_comments = logical_lines(stub_cell["source"])
             solu_code, solu_comments = logical_lines(cell["source"])
 
+            # Identify violations in the exercise cell
             unmatched_code = unmatched_lines(stub_code, solu_code)
             unmatched_comments = unmatched_lines(
                 stub_comments, solu_code + solu_comments
             )
 
-            print("-" * 60)
+            # Report the outcome for this exercise
+            print("-" * 69)
             print(f"Exercise {exercise}")
             report("Code", unmatched_code)
             report("Comment", unmatched_comments)
+            if unmatched_code or unmatched_comments:
+                failure = True
+
+    # Print overall summary and exit with return code
+    message = "Failure" if failure else "Success"
+    print("=" * 30, message, "=" * 30)
+    sys.exit(failure)
 
 
 def report(kind, unmatched, thresh=50):
-
-    if not unmatched:
-        print(f"{kind}: All perfect")
+    """Print information about code or comments in an exercise."""
+    if unmatched:
+        print(f"{kind}: FAIL")
+    else:
+        print(f"{kind}: PASS")
     for (score, stub, solu) in unmatched:
         if score < thresh:
-            print(f"{kind} without close match:")
-            print(f"* {stub}")
+            print(f" {kind} without close match:")
+            print(f" * {stub}")
         else:
-            print(f"{kind} with close mismatch ({score}%)")
-            print(f"+ {stub}")
-            print(f"- {solu}")
+            print(f" {kind} with close mismatch ({score}%)")
+            print(f" + {stub}")
+            print(f" - {solu}")
 
 
 def logical_lines(func_str):
-
+    """Extract code and block comments from cell string."""
     # Standardize docstring string format
     func_str = func_str.replace("'''", '"""')
 
@@ -69,10 +109,10 @@ def logical_lines(func_str):
 
     for line in func_str.split("\n"):
 
-        # Detect and ignore non-assigned block strings:
+        # Detect and ignore lines within two kinds of multi-line comments
         # - triple quotes (docstrings)
         # - comment hashmark fences
-        comment_block_fence = dedent(line).startswith('"""') or "#####" in line
+        comment_block_fence = dedent(line).startswith('"""') or "###" in line
         if reading_block_comment:
             if comment_block_fence:
                 reading_block_comment = False
@@ -85,12 +125,12 @@ def logical_lines(func_str):
         match = pattern.match(line)
         if match:
 
-            # Use nonn-commented component of the line
+            # Use nonn-commented component of a lie with inline comments
             code_line = match.group(1)
             comment_line = match.group(2)
 
             # Handle xkcd context, which is always last thing in solution cell
-            if "with plt.xkcd()" in code_line:
+            if "plt.xkcd()" in code_line:
                 making_xkcd_plot = True
                 continue
             if making_xkcd_plot:
@@ -108,21 +148,25 @@ def logical_lines(func_str):
 
 
 def unmatched_lines(stub_lines, solu_lines):
-
+    """Identify lines in the exercise stub without a match in the solution."""
     unmatched = []
 
     for stub_line in stub_lines:
 
+        # When we don't match, we want to track lines that are close
         best_score = 0
         best_line = ""
 
         for line in solu_lines:
 
+            # Match whole lines or parts of lines that need completion
             if "..." in stub_line:
                 part_scores = []
                 for part in stub_line.split("..."):
+                    if not part:
+                        continue
                     part_scores.append(fuzz.partial_ratio(part, line))
-                score = max(part_scores)
+                score = min(part_scores)
             else:
                 score = fuzz.ratio(stub_line, line)
 
@@ -130,6 +174,7 @@ def unmatched_lines(stub_lines, solu_lines):
                 best_score = score
                 best_line = line
 
+        # Track all lines that are not perfect matches
         if best_score < 100:
             unmatched.append((best_score, stub_line, best_line))
 
@@ -137,13 +182,13 @@ def unmatched_lines(stub_lines, solu_lines):
 
 
 def skip_code(line):
-
+    """Return True if a code line should be skipped based on contents."""
     line = dedent(line)
     return not line or "NotImplementedError" in line
 
 
 def skip_comment(line):
-
+    """Return True if a comment line should be skipped based on contents."""
     line = dedent(line)
     return not line or "to_remove" in line or "uncomment" in line.lower()
 
