a5cb4a3c1b2da95f08f44112fda5e41d90188220
GitHub Action <action@github.com>
2020-07-17 22:40:34 +0000

    Process tutorial notebooks

--- tutorials/W2D1_BayesianStatistics/solutions/W2D1_Tutorial2_Solution_4e129f5a.py
+++
@@ -1,9 +0,0 @@
-
-posterior_matrix = np.zeros_like(likelihood_matrix)
-
-for i_posterior in np.arange(posterior_matrix.shape[0]):
-    posterior_matrix[i_posterior,:] = prior_matrix[i_posterior,:] * likelihood_matrix[i_posterior,:]
-    posterior_matrix[i_posterior,:] = posterior_matrix[i_posterior,:] / np.sum(posterior_matrix[i_posterior,:])
-    
-with plt.xkcd():
-    plot_mymatrix(x, posterior_matrix, 'Orientation (Degree)', 'Repetitions', 'Sample Output')
