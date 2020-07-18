380c86879fe687f12686053497a9fde1d3c4fd1f
Michael Waskom <mwaskom@users.noreply.github.com>
2020-07-18 12:00:25 -0400

    W3D1 Final tutorials (rebased) (#377)
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Process tutorial notebooks
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Process tutorial notebooks
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Process tutorial notebooks
    
    * Process tutorial notebooks
    
    Co-authored-by: Spiros Ch <spirosChv@users.noreply.github.com>
    Co-authored-by: GitHub Action <action@github.com>

--- tutorials/W3D1_RealNeurons/solutions/W3D1_Tutorial1_Solution_7d86d7ff.py
+++
@@ -1,9 +0,0 @@
-pars = default_pars(T=100.)
-sig_gwn = 5.
-mu_gwn  = 250.
-I_GWN = my_GWN(pars, mu = mu_gwn, sig=sig_gwn, myseed=2020)
-v, sp = run_LIF(pars, I=I_GWN)
-
-with plt.xkcd():
-  plot_GWN(pars, I_GWN)
-  plt.show()
