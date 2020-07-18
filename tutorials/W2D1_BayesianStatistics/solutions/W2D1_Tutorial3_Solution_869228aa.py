f3c36f43ab53f997c3ce58e045ba1dfd0b10b707
GitHub Action <action@github.com>
2020-07-18 10:32:58 +0000

    Process tutorial notebooks

--- tutorials/W2D1_BayesianStatistics/solutions/W2D1_Tutorial3_Solution_869228aa.py
+++ tutorials/W2D1_BayesianStatistics/solutions/W2D1_Tutorial3_Solution_869228aa.py
@@ -8,5 +8,4 @@ def calculate_posterior_array(prior_array, likelihood_array):
 posterior_array = calculate_posterior_array(prior_array, likelihood_array)
 with plt.xkcd():
   plot_myarray(posterior_array, 'posterior: $p(x | \~x)$',
-               'Hypothetical True Stimulus $x$',
-               'Posterior as a fcn of $x$ : $p(x | \~x)$')
+  'Hypothetical True Stimulus $x$', 'Posterior as a fcn of $x$ : $p(x | \~x)$')
