42e2679d52de2ed9a90d676824e81db4bc964a85
GitHub Action <action@github.com>
2020-07-24 18:47:40 +0000

    Process tutorial notebooks

--- tutorials/W3D1_RealNeurons/solutions/W3D1_Tutorial1_Solution_14e10901.py
+++ tutorials/W3D1_RealNeurons/solutions/W3D1_Tutorial1_Solution_14e10901.py
@@ -1,8 +1,10 @@
 pars = default_pars(T=100.)
 sig_gwn = 5.
 mu_gwn = 250.
+
 # Calculate the GWN current
 I_GWN = my_GWN(pars, mu=mu_gwn, sig=sig_gwn, myseed=2020)
+
 # Run the model and calculate the v and the sp
 v, sp = run_LIF(pars, Iinj=I_GWN)
 
