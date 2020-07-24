975727b89c90b29bd955666b4742012956d1161b
GitHub Action <action@github.com>
2020-07-24 23:39:05 +0000

    Process tutorial notebooks

--- tutorials/W3D5_DeepLearning2/solutions/W3D5_Tutorial1_Solution_bd5d9545.py
+++
@@ -1,9 +0,0 @@
-
-encoding_size = 16
-
-model = nn.Sequential(nn.Linear(input_size, encoding_size),
-                      nn.PReLU(),
-                      nn.Linear(encoding_size, input_size),
-                      nn.Sigmoid())
-
-print(model)
