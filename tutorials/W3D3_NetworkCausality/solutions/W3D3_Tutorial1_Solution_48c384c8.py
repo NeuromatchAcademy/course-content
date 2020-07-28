d5ae89090d9bfc15a68c85b7508d7313d1827b26
GitHub Action <action@github.com>
2020-07-28 01:21:40 +0000

    Process tutorial notebooks

--- tutorials/W3D3_NetworkCausality/solutions/W3D3_Tutorial1_Solution_48c384c8.py
+++ tutorials/W3D3_NetworkCausality/solutions/W3D3_Tutorial1_Solution_48c384c8.py
@@ -1,6 +1,5 @@
 def simulate_neurons(A, timesteps, random_state=42):
-    """
-    Simulates a dynamical system for the specified number of neurons and timesteps.
+    """Simulates a dynamical system for the specified number of neurons and timesteps.
 
     Args:
         A (np.array): the connectivity matrix
@@ -10,32 +9,36 @@ def simulate_neurons(A, timesteps, random_state=42):
     Returns:
         - X has shape (n_neurons, timeteps). A schematic:
                    ___t____t+1___
-                  |   0    1     |
+       neuron  0  |   0    1     |
                   |   1    0     |
-       n_neurons  |   0 -> 1     |
+       neuron  i  |   0 -> 1     |
                   |   0    0     |
                   |___1____0_____|
 
     """
     np.random.seed(random_state)
-    
+
     n_neurons = len(A)
     X = np.zeros((n_neurons, timesteps))
 
     for t in range(timesteps-1):
-        # solution
-        epsilon = np.random.multivariate_normal(np.zeros(n_neurons), np.eye(n_neurons))
-        X[:, t+1] = sigmoid(A.dot(X[:,t]) + epsilon) # we are using helper function sigmoid
 
-        assert epsilon.shape == (n_neurons,)
+        # Create noise vector
+        epsilon = np.random.multivariate_normal(np.zeros(n_neurons), np.eye(n_neurons))
+
+        # Update activity vector for next step
+        X[:, t+1] = sigmoid(A @ X[:,t] + epsilon) # we are using helper function sigmoid
+
     return X
 
-### Now test it
-timesteps = 5000 # Simulate for 5000 timesteps.
 
-# Simulate our dynamical system for the given amount of time
+# Set simulation length
+timesteps = 5000 
+
+# Uncomment below to test your function
+
+# Simulate our dynamical system 
 X = simulate_neurons(A, timesteps)
-with plt.xkcd():
-  f, ax = plt.subplots()
-  ax.imshow(X[:,:10])
-  ax.set(xlabel='Timestep', ylabel='Neuron', title='Simulated Neural Activity')
+
+with plt.xkcd():  
+  plot_neural_activity(X)
--- tutorials/W3D3_NetworkCausality/solutions/W3D3_Tutorial4_Solution_0308292a.py
+++ tutorials/W3D3_NetworkCausality/solutions/W3D3_Tutorial4_Solution_0308292a.py
@@ -20,23 +20,22 @@ def simulate_neurons_iv(n_neurons, timesteps, eta, random_state=42):
     X = np.zeros((n_neurons, timesteps))
     Z = np.random.choice([0,1], size=(n_neurons, timesteps))
     for t in range(timesteps-1):
-      # solution
+
       IV_on_this_timestep = (eta * Z[:,t+1])
-
-
+      
       X[:, t+1] = sigmoid(A.dot(X[:,t]) + IV_on_this_timestep + np.random.multivariate_normal(np.zeros(n_neurons), np.eye(n_neurons)))
 
     return A, X, Z
 
-# let's visualize the dynamics
+# Parameters
 timesteps = 5000 # Simulate for 5000 timesteps.
 n_neurons = 100 # the size of our system
 eta = 2 # the strength of our instrument, higher is stronger
+
+
+# Uncomment below to test your function
+
 # Simulate our dynamical system for the given amount of time
 A, X, Z = simulate_neurons_iv(n_neurons, timesteps, eta)
-with plt.xkcd():
-  plt.imshow(X[:, :10],aspect='auto')
-  plt.colorbar()
-  plt.ylabel("Neuron", fontsize=15)
-  plt.xlabel("Timestep", fontsize=15);
-  plt.title("Our dynamical system with an IV")
+with plt.xkcd():  
+  plot_neural_activity(X)
