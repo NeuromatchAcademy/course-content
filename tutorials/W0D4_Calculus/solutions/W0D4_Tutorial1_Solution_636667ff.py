12d70f05806c35c5f89ed1fa79b52689ef802876
Ella Batty <erb2180@columbia.edu>
2021-06-27 17:28:15 -0400

    Initial (post pre-pod) waxing of W0D4 (#626)
    * Initial post pre-pod waxing
    
    * Initial post pre-pod waxing
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>

--- tutorials/W0D4_Calculus/solutions/W0D4_Tutorial1_Solution_636667ff.py
+++ tutorials/W0D4_Calculus/solutions/W0D4_Tutorial1_Solution_636667ff.py
@@ -17,6 +17,6 @@ dv_dt = -1/tau * np.exp(-t/tau)
 # Define full derivative
 df_dt = u_t * dv_dt + v_t * du_dt
 
-# Uncomment below to visualize
+# Visualize
 with plt.xkcd():
   plot_alpha_func(t, f, df_dt)
---
+++ tutorials/W0D4_Calculus/solutions/W0D4_Tutorial2_Solution_fba302c0.py
@@ -0,0 +1,4 @@
+
+"""
+dV/dt becomes bigger and less of it is below 0. This means the solution will increase well beyond what is bioligically plausible
+"""
