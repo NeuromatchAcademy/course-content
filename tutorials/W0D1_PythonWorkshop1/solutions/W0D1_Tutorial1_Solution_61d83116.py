b33a151b02b08255f68e5b78fbcb15cfc934ee2b
Ella Batty <erb2180@columbia.edu>
2021-06-27 00:55:18 -0400

    Update to 2021 version (#621)
    * Update to 2021 version
    
    * Update to 2021 version
    
    * Fixed exercise problem
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>

--- tutorials/W0D1_PythonWorkshop1/solutions/W0D1_Tutorial1_Solution_61d83116.py
+++ tutorials/W0D1_PythonWorkshop1/solutions/W0D1_Tutorial1_Solution_61d83116.py
@@ -1,28 +1,34 @@
-# set random number generator
+
+# Set random number generator
 np.random.seed(2020)
 
-# initialize step_end, n, t_range, v and syn
+# Initialize step_end, n, t_range, v and syn
 step_end = int(t_max / dt)
 n = 50
 t_range = np.linspace(0, t_max, num=step_end)
 v_n = el * np.ones([n, step_end])
-syn = i_mean * (1 + 0.1 * (t_max / dt)**(0.5) * (2 * np.random.random([n, step_end]) - 1))
+i = i_mean * (1 + 0.1 * (t_max / dt)**(0.5) * (2 * np.random.random([n, step_end]) - 1))
 
-# loop for step_end - 1 steps
+# Loop for step_end - 1 steps
 for step in range(1, step_end):
-  v_n[:,step] = v_n[:,step - 1] + (dt / tau) * (el - v_n[:, step - 1] + r * syn[:, step])
 
+   # Compute v_n
+   v_n[:, step] = v_n[:, step - 1] + (dt / tau) * (el - v_n[:, step - 1] + r * i[:, step])
+
+# Compute sample mean (use np.mean)
 v_mean = np.mean(v_n, axis=0)
+
+# Compute sample standard deviation (use np.std)
 v_std = np.std(v_n, axis=0)
 
+# Plot figure
 with plt.xkcd():
-  # initialize the figure
   plt.figure()
   plt.title('Multiple realizations of $V_m$')
   plt.xlabel('time (s)')
   plt.ylabel('$V_m$ (V)')
 
-  plt.plot(t_range, v_n[:-1].T, 'k', alpha=0.3)
+  plt.plot(t_range, v_n.T, 'k', alpha=0.3)
 
   plt.plot(t_range, v_n[-1], 'k', alpha=0.3, label='V(t)')
   plt.plot(t_range, v_mean, 'C0', alpha=0.8, label='mean')
