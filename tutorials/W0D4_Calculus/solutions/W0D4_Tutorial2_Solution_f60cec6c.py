90b0f73a1ad2078dd00373820b9d11de816b9c53
GitHub Action <action@github.com>
2021-06-30 20:25:38 +0000

    Process tutorial notebooks

---
+++ tutorials/W0D4_Calculus/solutions/W0D4_Tutorial2_Solution_f60cec6c.py
@@ -0,0 +1,5 @@
+
+"""
+  1. As $I$ increases, the number of spikes increases.
+  2. No, as there is a limit to the number of spikes due to a refractory period, which is not accounted for in this model.
+"""
