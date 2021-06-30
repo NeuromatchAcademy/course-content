7b988c1db7eb6be8081645f19dfe76b061a775b3
Ella Batty <erb2180@columbia.edu>
2021-06-30 16:49:04 -0400

    Wax of W0D4 (#664)
    * Add demos to W0D4 T1
    
    * Process tutorial notebooks
    
    * Add timings
    
    * Process tutorial notebooks
    
    * Add timings
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>

--- tutorials/W0D4_Calculus/solutions/W0D4_Tutorial1_Solution_5deca1d0.py
+++
@@ -1,7 +0,0 @@
-
-"""
-2) Change a term involving y to involve more than y or y^2. For example, x**2 +  2*x*y + y**3
-
-3) If there are no terms involving both x and y, the partial derivative with respect to x will
-   just depend on x and the partial derivative with respect to y will just depend on y.
-"""
