36843a33acde952013f1ebc4032d9ff241e0721b
GitHub Action <action@github.com>
2021-06-30 11:10:38 +0000

    Process tutorial notebooks

--- tutorials/W0D3_LinearAlgebra/solutions/W0D3_Tutorial1_Solution_556934c8.py
+++ tutorials/W0D3_LinearAlgebra/solutions/W0D3_Tutorial1_Solution_556934c8.py
@@ -7,6 +7,6 @@
 
 # 3) The span of a, b, and c is a 2D plane through all of 3D space (note this is not R^2)
 
-# 4) The span of a and b is a 1D line through 3D space (note this is not R^1)
+# 4) The span of a is a 1D line through 3D space (note this is not R^1)
 
 # 5) The span of a and b is a 2d plane through 3D space (note this is not R^2)
