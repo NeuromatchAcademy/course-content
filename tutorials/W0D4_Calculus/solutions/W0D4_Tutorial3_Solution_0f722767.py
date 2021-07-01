f95ee6d0f8532981b9348ceba59e18d322d42a94
Ella Batty <erb2180@columbia.edu>
2021-07-01 07:39:27 -0400

    W0D4 fixes (#669)
    * Fix exercise discussion
    
    * Process tutorial notebooks
    
    * Add clarification
    
    * Created using Colaboratory
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>

---
+++ tutorials/W0D4_Calculus/solutions/W0D4_Tutorial3_Solution_0f722767.py
@@ -0,0 +1,10 @@
+
+"""
+  1. Personal preference: both have their benefits
+  2. The solution is stable.
+  3. The quadrants are:
+    - Top right positive derivative for inhibition and  negative for excitation (r_I increases, r_E decreases)
+    - top left negative derivative for both inhibition and excitation (r_I decrease, r_E decrease)
+    - bottom left negative derivative for inhibition and  positive for excitation (r_I decrease, r_E increase)
+    - bottom right positive derivative for inhibition and excitation (r_I increase, r_E increase)
+"""
