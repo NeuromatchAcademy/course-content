3c683308bdf40778345386691049a507b5fe29e9
GitHub Action <action@github.com>
2021-07-02 11:49:18 +0000

    Process tutorial notebooks

--- tutorials/W0D5_Statistics/solutions/W0D5_Tutorial2_Solution_cf9bfae6.py
+++ tutorials/W0D5_Statistics/solutions/W0D5_Tutorial2_Solution_cf9bfae6.py
@@ -1,4 +1,19 @@
 
+# We define the function to optimise, the negative log likelihood
+def negLogLike(theta, x):
+  """ Function for computing the negative log-likelihood given the observed data
+  and given parameter values stored in theta.
+
+    Args:
+      theta (ndarray): normal distribution parameters (mean is theta[0],
+                            variance is theta[1])
+      x (ndarray): array with observed data points
+
+    Returns:
+      Calculated negative Log Likelihood value!
+  """
+  return -sum(np.log(norm.pdf(x, theta[0], theta[1])))
+
 # Set random seed
 np.random.seed(0)
 
@@ -8,25 +23,11 @@ true_standard_dev = 1
 n_samples = 1000
 x = np.random.normal(true_mean, true_standard_dev, size = (n_samples,))
 
-# We define the function to optimise, the negative log likelihood
-def negLogLike(theta):
-  """ Function for computing the negative log-likelihood given the observed data
-  and given parameter values stored in theta.
-
-    Args:
-      theta (ndarray): normal distribution parameters (mean is theta[0],
-                            variance is theta[1])
-
-    Returns:
-      Calculated negative Log Likelihood value!
-  """
-  return -sum(np.log(norm.pdf(x, theta[0], theta[1])))
-
 # Define bounds, var has to be positive
 bnds = ((None, None), (0, None))
 
 # Optimize with scipy!
-optimal_parameters = sp.optimize.minimize(negLogLike, (2, 2), bounds = bnds)
+optimal_parameters = sp.optimize.minimize(negLogLike, (2, 2), args = x, bounds = bnds)
 print("The optimal mean estimate is: " + str(optimal_parameters.x[0]))
 print("The optimal variance estimate is: " + str(optimal_parameters.x[1]))
 
