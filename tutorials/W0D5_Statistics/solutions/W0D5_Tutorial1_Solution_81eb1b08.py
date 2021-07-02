72427eebf0033a5ef766e94be59f3920649a5b16
Ella Batty <erb2180@columbia.edu>
2021-07-02 07:26:17 -0400

    W0D5 fix (#676)
    * Created using Colaboratory
    
    * Fix solution cell
    
    * Created using Colaboratory
    
    * Sync comments
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>

--- tutorials/W0D5_Statistics/solutions/W0D5_Tutorial1_Solution_81eb1b08.py
+++ tutorials/W0D5_Statistics/solutions/W0D5_Tutorial1_Solution_81eb1b08.py
@@ -1,9 +1,6 @@
 
 """
 1) More samples means the distribution starts to look more symmetric.
- This is a result of the Central Limit Theorem, where a large number
-of sampled values makes samples from the Poisson distribution look similarly
-distributed to the symmetric Gaussian (Normal) distribution.
 
 2) Increasing lambda moves the distribution along the x-axis, essentialy changing the
    mean of the distribution. With lambda = 6 for example, we would expect to often see
