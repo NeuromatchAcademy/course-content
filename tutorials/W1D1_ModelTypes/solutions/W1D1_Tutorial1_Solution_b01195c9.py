c9dd69986e8ff69aadf393c8e7ba4d124da84e56
GitHub Action <action@github.com>
2021-07-03 16:34:43 +0000

    Process tutorial notebooks

---
+++ tutorials/W1D1_ModelTypes/solutions/W1D1_Tutorial1_Solution_b01195c9.py
@@ -0,0 +1,5 @@
+
+"""
+The above histogram is the distribution (across neurons) of the cumulative sum of
+all individual neurons' total spike count, i.e. sum across each row in the raster plot.
+"""
