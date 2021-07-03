3a2bb3fc6e8976aa1f93968c3c6957473ee9a18b
GitHub Action <action@github.com>
2021-07-03 17:26:15 +0000

    Process tutorial notebooks

--- tutorials/W1D1_ModelTypes/solutions/W1D1_Tutorial2_Solution_9e5a4843.py
+++ tutorials/W1D1_ModelTypes/solutions/W1D1_Tutorial2_Solution_9e5a4843.py
@@ -1,3 +1,4 @@
+
 def lif_neuron(n_steps=1000, alpha=0.01, rate=10):
   """ Simulate a linear integrate-and-fire neuron.
 
@@ -5,17 +6,23 @@ def lif_neuron(n_steps=1000, alpha=0.01, rate=10):
     n_steps (int): The number of time steps to simulate the neuron's activity.
     alpha (float): The input scaling factor
     rate (int): The mean rate of incoming spikes
+
   """
-  # precompute Poisson samples for speed
+  # Precompute Poisson samples for speed
   exc = stats.poisson(rate).rvs(n_steps)
 
+  # Initialize voltage and spike storage
   v = np.zeros(n_steps)
   spike_times = []
+
+  # Loop over steps
   for i in range(1, n_steps):
 
+    # Update v
     dv = alpha * exc[i]
-
     v[i] = v[i-1] + dv
+
+    # If spike happens, reset voltage and record
     if v[i] > 1:
       spike_times.append(i)
       v[i] = 0
@@ -25,6 +32,9 @@ def lif_neuron(n_steps=1000, alpha=0.01, rate=10):
 # Set random seed (for reproducibility)
 np.random.seed(12)
 
+# Model LIF neuron
 v, spike_times = lif_neuron()
+
+# Visualize
 with plt.xkcd():
   plot_neuron_stats(v, spike_times)
