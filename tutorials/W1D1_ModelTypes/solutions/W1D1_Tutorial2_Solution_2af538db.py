e9351163a6096c0594a95d1f354c027b6f558422
Marius Pachitariu <marius10p@gmail.com>
2021-07-04 02:24:46 +0300

    updated book project chapters skip ci (#702)
    * W1D1 2021 updates (#691)
    
    * 2021 updates
    
    * Process tutorial notebooks
    
    * 2021 update to W1D1 T2
    
    * 2021 updates
    
    * Remove reference to appendix
    
    * Process tutorial notebooks
    
    * Update to 2021 version
    
    * Add prereqs knowledge
    
    * Refresh kernel
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>
    
    * Add notation sections to W1D1 (#696)
    
    * Add notation section
    
    * Add notation section
    
    * Add notation
    
    * Add notation section
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>
    
    * Fix bilibili link (#698)
    
    * W1D1 add discussion tutorial (#697)
    
    * Add discussion tutorial
    
    * Update materials.yml
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>
    
    * Add tech help chapter cover (#699)
    
    Co-authored-by: Ella Batty <erb2180@columbia.edu>
    Co-authored-by: GitHub Action <action@github.com>

---
+++ tutorials/W1D1_ModelTypes/solutions/W1D1_Tutorial2_Solution_2af538db.py
@@ -0,0 +1,10 @@
+
+"""
+1. The spikes are pretty predictable - they occur at fairly regular intervals.
+2. Raising alpha increases the rate of spikes (and thus decreases the inter-spike intervals)
+3. Raising the rate increases the rate of spikes (and thus decreases the inter-spike intervals)
+These two facts make sense because they both control the size of the input affecting the neuron - more
+input means higher responses.
+4. No, the ISIs look pretty symmetrical and Gaussian (not exponential)
+
+"""
