d25de48868955b305f95f7873cfbef585302ecee
GitHub Action <action@github.com>
2021-07-04 07:17:11 +0000

    Process tutorial notebooks

--- tutorials/W1D4_GeneralizedLinearModels/solutions/W1D4_Tutorial2_Solution_89590c8d.py
+++ tutorials/W1D4_GeneralizedLinearModels/solutions/W1D4_Tutorial2_Solution_89590c8d.py
@@ -1,5 +1,11 @@
 def sigmoid(z):
   """Return the logistic transform of z."""
-  return 1 / (1 + np.exp(-z))
+
+  sigmoid = 1 / (1 + np.exp(-z))
+
+  return sigmoid
+
+
+# Visualize
 with plt.xkcd():
   plot_function(sigmoid, "\sigma", "z", (-10, 10))
