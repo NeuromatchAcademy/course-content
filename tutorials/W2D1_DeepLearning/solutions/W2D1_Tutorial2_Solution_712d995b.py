ab307b007c8ff4eae2cadf7273deb6bbec645b16
Ella Batty <erb2180@columbia.edu>
2021-07-11 19:54:51 -0400

    W2D1 fixes (#763)
    * Created using Colaboratory
    
    * Add timings
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>

--- tutorials/W2D1_DeepLearning/solutions/W2D1_Tutorial2_Solution_712d995b.py
+++ tutorials/W2D1_DeepLearning/solutions/W2D1_Tutorial2_Solution_712d995b.py
@@ -11,15 +11,14 @@ example_filters = filters(out_channels, K) # create filters to use
 
 convout = np.zeros(0) # assign convolutional activations to convout
 
-### Initialize conv layer with weights from function filters
+# Initialize conv layer and add weights from function filters
 # you need to specify :
 # * the number of input channels c_in
 # * the number of output channels c_out
 # * the filter size K
-convLayer = ConvolutionalLayer(c_in=in_channels, c_out=out_channels, K=K,
-                               filters=example_filters)
+convLayer = ConvolutionalLayer(c_in=in_channels, c_out=out_channels, K=K, filters=example_filters)
 
-### Create stimuli (H_in, W_in)
+# Create stimuli (H_in, W_in)
 orientations = [-90, -45, 0, 45, 90]
 stimuli = torch.zeros((len(orientations), in_channels, h, w), dtype=torch.float32)
 for i,ori in enumerate(orientations):
