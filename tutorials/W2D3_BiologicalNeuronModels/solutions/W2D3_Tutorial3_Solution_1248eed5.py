ca981d81061352c3507f151a6bee971014337725
GitHub Action <action@github.com>
2021-07-13 23:20:35 +0000

    Process tutorial notebooks

--- tutorials/W2D3_BiologicalNeuronModels/solutions/W2D3_Tutorial3_Solution_1248eed5.py
+++ tutorials/W2D3_BiologicalNeuronModels/solutions/W2D3_Tutorial3_Solution_1248eed5.py
@@ -1,4 +1,7 @@
+
+# Get default parameters
 pars = default_pars(T=1000.)
+
 # Add parameters
 pars['gE_bar'] = 2.4    # [nS]
 pars['VE'] = 0.         # [mV] excitatory reversal potential
@@ -7,13 +10,15 @@ pars['gI_bar'] = 2.4    # [nS]
 pars['VI'] = -80.       # [mV] inhibitory reversal potential
 pars['tau_syn_I'] = 5.  # [ms]
 
-# generate presynaptic spike trains
+# Generate presynaptic spike trains
 pre_spike_train_ex = Poisson_generator(pars, rate=10, n=80)
 pre_spike_train_in = Poisson_generator(pars, rate=10, n=20)
 
-# simulate conductance-based LIF model
+# Simulate conductance-based LIF model
 v, rec_spikes, gE, gI = run_LIF_cond(pars, 0, pre_spike_train_ex,
                                      pre_spike_train_in)
+
+# Show spikes more clearly by setting voltage high
 dt, range_t = pars['dt'], pars['range_t']
 if rec_spikes.size:
   sp_num = (rec_spikes / dt).astype(int) - 1
@@ -21,6 +26,7 @@ if rec_spikes.size:
 
 # Change the threshold
 pars['V_th'] = 1e3
+
 # Calculate FMP
 v_fmp, _, _, _ = run_LIF_cond(pars, 0, pre_spike_train_ex, pre_spike_train_in)
 
