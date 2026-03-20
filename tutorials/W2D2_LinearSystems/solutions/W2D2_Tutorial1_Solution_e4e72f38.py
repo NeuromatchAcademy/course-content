e5bbeb4d003e235879fe1762ea6f2038f73cecc3
Konstantine Tsafatinos <kosta.tsaf@gmail.com>
2026-03-20 00:54:49 -0400

    rename: W2D2_LinearSystems → W2D3_LinearSystems (2026 schedule)

--- tutorials/W2D2_LinearSystems/solutions/W2D2_Tutorial1_Solution_e4e72f38.py
+++
@@ -1,5 +0,0 @@
-"""
-Changing the initial conditions for this A always produces oscillatory
-dynamics. The only difference is the radii of the resulting elliptical
-trajectories.
-""";
--- tutorials/W2D2_LinearSystems/solutions/W2D2_Tutorial4_Solution_eca1c7e8.py
+++
@@ -1,5 +0,0 @@
-"""
-The corresponding autoregressive model is:
-
-x_{k+1} = 0 - x_{k}
-""";
